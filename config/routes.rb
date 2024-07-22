22f1098966990c2c8d368ed482e15ea5444b5339
Sebastiao Trindade <sebatrindade@hotmail.com>
2024-07-22 14:42:06 -0300

    Implantação da tela de login e área administrativa

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do  
+  resources :administradors
   resources :pedidos do
     resources :pedido_produtos
   end
   resources :clientes
   resources :produtos
-  resources :tipo_produtos
-  get '/home', to: 'home#index'
+  resources :tipo_produtos  
+  
+  get '/admin', to: 'admin#index'
+  get '/login', to: 'login#index'
+  post '/login/logar', to: 'login#logar'
+  get '/login/sair', to: 'login#sair'
+
   root to: 'home#index'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
